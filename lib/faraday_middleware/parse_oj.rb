67d3cb4d067d3eb2e2fb685a6cbb5b3626ca77ae
Andrey Yuferov <asjuferov@gmail.com>
2014-03-26 05:32:20 +0400

    Preparing for faraday 0.9

--- lib/faraday_middleware/parse_oj.rb
+++ lib/faraday_middleware/parse_oj.rb
@@ -3,13 +3,15 @@ require 'faraday_middleware/response_middleware'
 module FaradayMiddleware
   class ParseOj < ResponseMiddleware
     dependency 'oj'
-    
+
     define_parser do |body|
       Oj.load(body, mode: :compat) unless body.strip.empty?
     end
-    
+
     VERSION = '0.2.1'
   end
 end
 
-Faraday.register_middleware :response, oj: FaradayMiddleware::ParseOj
+if Faraday.respond_to? :register_middleware
+  Faraday.register_middleware :response, oj: FaradayMiddleware::ParseOj
+end
